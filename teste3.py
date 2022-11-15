425745bb81ffece1ede92cae8847509551a0f219
brendopk <116086132+brendopk@users.noreply.github.com>
2022-11-15 01:02:49 -0300

    Add files via upload

---
+++ teste3.py
@@ -0,0 +1,8 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Informe a sua idade: "))
+if idade >= 18:
+    print(f"{nome} você tem {idade} e é de maior!")
+else:
+    print(f"{nome} você é de menor")
+
+
